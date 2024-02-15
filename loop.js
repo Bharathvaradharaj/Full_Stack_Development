3ecaf967076bc9e50da70ea369f722f59e0e0ae2
Bharathvaradharaj <thilagasara788@gmail.com>
2024-02-15 08:37:50 +0530

    Greendent lunched

---
+++ loop.js
@@ -0,0 +1,4 @@
+for(count=2;count<=20;count=count*2){
+
+    console.log("1x2="+count)
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./tailwind_project/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
