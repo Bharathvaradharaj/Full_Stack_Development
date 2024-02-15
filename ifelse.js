c5f17e3a05b355d23472765dc3129bec97dded4e
Bharath-Magzter <bharath.v@magzter.com>
2024-02-15 19:30:20 +0530

    file name changed

---
+++ ifelse.js
@@ -0,0 +1,25 @@
+var season = "summer"
+
+if(season== "spring"){
+
+    "Enjoy the blooming flowers"
+}
+else if(season== "summmar")
+{
+
+
+    "Have fun in  the summer"
+}
+
+else if(season=="autumn"){
+
+"aDMIRE THE COLORFULL LEAVES"
+
+
+
+}
+
+else if(season=="winter"){
+
+    'Bindle up and stay warm'
+}
